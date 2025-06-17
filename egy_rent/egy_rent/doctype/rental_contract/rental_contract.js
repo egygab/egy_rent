2eeaea3a8e0f21d4ef356100bab9e552379f3f92
m <it@egygab.com>
2025-06-17 21:49:09 +0300

    rental contract v01

--- egy_rent/egy_rent/doctype/rental_contract/rental_contract.js
+++ egy_rent/egy_rent/doctype/rental_contract/rental_contract.js
@@ -1,8 +1,21 @@
 // Copyright (c) 2025, . and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Rental Contract", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Rental Contract", {
+	refresh(frm) {
+       frm.add_custom_button(__('Get User Email Address'), function() {
+           frappe.call({
+                    method: 'egy_rent.egy_rent.egy_rent.doctype.rental_contract.rental_contract.calc_contract_list',
+                    // Use args property to pass data to the python method
+                    // Python method: def print_msg(name, age):
+                    args: {name: 'some_name', age: 30},
+                    callback: function(ret) {
+                        // If you expect something to return form the call use the code bellow or replace it with your own code
+                        if (!ret || ret.message == null) return;
+                        let data = ret.message;
+                        
+                    }
+                });
+       }, __("Utilities"));
+	},
+});
--- egy_rent/egy_rent/doctype/rental_units/rental_units.py
+++ egy_rent/egy_rent/doctype/rental_units/rental_units.py
@@ -1,11 +1,9 @@
 # Copyright (c) 2025, . and contributors
 # For license information, please see license.txt
 
-import frappe
+# import frappe
 from frappe.model.document import Document
 
 
-class RentalContract(Document):
-	@frappe.whitelist()
-	def calc_contract_list():
-		frappe.msgprint(('Success'))
+class RentalUnits(Document):
+	pass
