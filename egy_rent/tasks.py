e737ff67333e5ca02b4c6ea095270571e79418e0
Mahmoud <m.naguib@egygab.com>
2025-11-03 16:33:19 +0200

    Add PostgreSQL support

--- egy_rent/tasks.py
+++ egy_rent/tasks.py
@@ -3,6 +3,9 @@ import requests
 import json
 import time
 from datetime import date, timedelta
+from frappe.utils.password import get_decrypted_password
+
+import psycopg2
 
 def foodics_response(endpoint,data={},headers={}):
     response = requests.request("GET", endpoint, headers=headers, data=data)
@@ -63,6 +66,9 @@ def pull_integration_invoices(business_date = date.today() - timedelta(days=1)):
     for store in stores:
         store_ = frappe.get_doc('Rental Integration Master', store.name)
         #print (store_.token)
+        ######################################################
+        ################### foodics ##########################
+        ######################################################
         if store_.integration_type == "foodics" :
             ################### foodics ##########################
             url = "https://api.foodics.com/v5/orders?filter[business_date]=" + str(business_date) + "&filter[branch_id]=" + str(store_.branch_id)
@@ -85,6 +91,44 @@ def pull_integration_invoices(business_date = date.today() - timedelta(days=1)):
                     _next_url = response.json().get('links').get('next')
                     if _next_url :
                         _next_url = response.json().get('links').get('next') + "&filter[business_date]="+ str(business_date) + "&filter[branch_id]=" + str(store_.branch_id)
+        if store_.integration_type == "PostgreSQL" :
+            
+            ##print (get_decrypted_password('Rental Integration Master',store.name,"db_password"))
+            try:
+            # Establish a connection to the PostgreSQL database
+                conn = psycopg2.connect(
+                    host= store_.db_host,          # Or your database host
+                    database= store_.db_name,  # Replace with your database name
+                    user= store_.db_username,      # Replace with your PostgreSQL username
+                    password= get_decrypted_password('Rental Integration Master',store.name,"db_password"), # Replace with your PostgreSQL password
+                    port= store_.db_port      # Optional: specify port if not default
+                )
+                cur = conn.cursor()
+                _sql = str( store_.sql_view ) + "'" + str(business_date) + "'"
+                print (_sql)
+                cur.execute( _sql )
+                invis = cur.fetchall()
+                for inv in invis:
+                    new_si = frappe.new_doc("Rental Integration Sales")
+                    new_si.store = store_.name
+                    new_si.external_id = str(store_.name) + "-" + str(inv[0])
+                    new_si.external_reference = str(inv[0])
+                    new_si.invoice_date = business_date
+                    new_si.net_amount = inv[2]
+                    new_si.invoice_amount = inv[3]
+                    new_si.external_type_name="Active"
+                    new_si.insert()
+                    frappe.db.commit()
+                
+            except psycopg2.Error as e:
+                print(f"Error connecting to PostgreSQL: {e}")
+            finally:
+                # Close the cursor and connection
+                if cur:
+                    cur.close()
+                if conn:
+                    conn.close()
+                print("Database connection closed.")                
 
     print ("=============== >> " + str(business_date))
             #print(response.json())
